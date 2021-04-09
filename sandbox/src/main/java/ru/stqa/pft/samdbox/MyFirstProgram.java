2e7256195eef201a7ac1a80666446fef6d9529f4
raik.tatyana <raik.tatyana@gmail.com>
2021-04-09 21:29:02 +0300

    functions

--- sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
@@ -2,14 +2,26 @@ package ru.stqa.pft.samdbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    String somebody = "World";
-    System.out.println("Hello " + somebody +"!");
-    int a = 8;
-    int s = a*a;
-    System.out.println("Плащадь квадрата со стороной " + a + " = " + s);
+    greeting("Mike");
+    greeting("Spike");
+    greeting("Nike");
 
-    double b = 8;
-    double c = b*b;
-    System.out.println("Плащадь квадрата со стороной " + b + " = " + c);
+    double len = 5;
+    System.out.println("Площадь квадрата со стороной " + len + " =" + area(len));
+    double a = 5;
+    double b = 4;
+    System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " =" + area(a, b));
+  }
+
+  public static void greeting(String name){
+    System.out.println("Hello, "  + name + '!');
+  }
+
+  public static double area(double val){
+    return val * val;
+  }
+
+  public static double area(double a, double b){
+    return a * b;
   }
 }

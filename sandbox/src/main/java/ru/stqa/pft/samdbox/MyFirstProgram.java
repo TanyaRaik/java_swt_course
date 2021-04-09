56ee2fca80db9c9080ad56e38da01de616cec4a3
raik.tatyana <raik.tatyana@gmail.com>
2021-04-09 21:04:02 +0300

    calculations

--- sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
@@ -3,16 +3,8 @@ package ru.stqa.pft.samdbox;
 public class MyFirstProgram {
   public static void main(String[] args) {
     System.out.println("Hello World");
-    System.out.println(2 + 2);
-    System.out.println(2 - 2);
-    System.out.println(2 * 2);
-    System.out.println(2 / 2);
-    System.out.println(1 / 2);
-    System.out.println(1.0 / 2);
-
-
-    System.out.println("2" + "2");
-    System.out.println("2" + 2);
-    System.out.println("2 + 2 = " + (2 + 2));
+    int a = 8;
+    int s = a*a;
+    System.out.println("Плащадь квадрата со стороной " + a + " = " + s);
   }
 }

49f46f4d229dcc2b433b08a8818d3f1e9c9a3c36
raik.tatyana <raik.tatyana@gmail.com>
2021-04-09 21:10:01 +0300

    data types

--- sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
@@ -2,9 +2,14 @@ package ru.stqa.pft.samdbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    String somebody = "World";
+    System.out.println("Hello " + somebody +"!");
     int a = 8;
     int s = a*a;
     System.out.println("Плащадь квадрата со стороной " + a + " = " + s);
+
+    double b = 8;
+    double c = b*b;
+    System.out.println("Плащадь квадрата со стороной " + b + " = " + c);
   }
 }

685401c7be7ea54e4f3e76115d73a43792b9b96e
raik.tatyana <raik.tatyana@gmail.com>
2021-04-18 11:07:30 +0300

    homework4: refactored groupsTest and added userTest

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -4,7 +4,6 @@ import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class GroupCreationTests {
   private WebDriver wd;
@@ -14,93 +13,51 @@ public class GroupCreationTests {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
+    login("admin", "secret");
+  }
+
+  private void login(String admin, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(admin);
     wd.findElement(By.id("LoginForm")).click();
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
   }
 
   @Test
   public void testGroupCreation() throws Exception {
-    wd.findElement(By.linkText("add new")).click();
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-    wd.findElement(By.name("firstname")).sendKeys("name");
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys("middle name");
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys("last name");
-    wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys("nickname");
-    wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys("title");
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys("company");
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys("address");
-    wd.findElement(By.name("home")).click();
-    wd.findElement(By.name("home")).clear();
-    wd.findElement(By.name("home")).sendKeys("home");
-    wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys("middle");
-    wd.findElement(By.name("work")).click();
-    wd.findElement(By.name("work")).clear();
-    wd.findElement(By.name("work")).sendKeys("work");
-    wd.findElement(By.name("fax")).click();
-    wd.findElement(By.name("fax")).clear();
-    wd.findElement(By.name("fax")).sendKeys("111");
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys("raik.tatyana@gmail.com");
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys("raik2@gmail.com");
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("email3")).click();
-    wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys("Raik3@gmail.com");
-    wd.findElement(By.name("homepage")).click();
-    wd.findElement(By.name("homepage")).clear();
-    wd.findElement(By.name("homepage")).sendKeys("homepage");
-    wd.findElement(By.name("bday")).click();
-    new Select(wd.findElement(By.name("bday"))).selectByVisibleText("1");
-    wd.findElement(By.name("bday")).click();
-    wd.findElement(By.name("bmonth")).click();
-    new Select(wd.findElement(By.name("bmonth"))).selectByVisibleText("January");
-    wd.findElement(By.name("bmonth")).click();
-    wd.findElement(By.name("byear")).click();
-    wd.findElement(By.name("byear")).clear();
-    wd.findElement(By.name("byear")).sendKeys("1992");
-    wd.findElement(By.name("theform")).click();
-    wd.findElement(By.name("aday")).click();
-    new Select(wd.findElement(By.name("aday"))).selectByVisibleText("2");
-    wd.findElement(By.name("aday")).click();
-    wd.findElement(By.name("amonth")).click();
-    new Select(wd.findElement(By.name("amonth"))).selectByVisibleText("January");
-    wd.findElement(By.name("amonth")).click();
-    wd.findElement(By.name("new_group")).click();
-    wd.findElement(By.name("new_group")).click();
-    wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys("address2");
-    wd.findElement(By.name("phone2")).click();
-    wd.findElement(By.name("phone2")).clear();
-    wd.findElement(By.name("phone2")).sendKeys("home2");
-    wd.findElement(By.name("notes")).click();
-    wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys("notes");
-    wd.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("q", "q", "q"));
+    submitGroupCreation();
+    goToGroupPage();
+  }
+
+  private void submitGroupCreation() {
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void fillGroupForm(GroupData groupData) {
+    wd.findElement(By.name("group_name")).click();
+    wd.findElement(By.name("group_name")).clear();
+    wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+    wd.findElement(By.name("group_header")).click();
+    wd.findElement(By.name("group_header")).clear();
+    wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+    wd.findElement(By.name("group_footer")).click();
+    wd.findElement(By.name("group_footer")).clear();
+    wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+  }
+
+  private void initGroupCreation() {
+    wd.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
+    wd.findElement(By.linkText("groups")).click();
   }
 
   @AfterMethod(alwaysRun = true)
---
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/UserData.java
@@ -0,0 +1,121 @@
+package ru.stqa.pft.addressbook;
+
+public class UserData {
+  private final String firstName;
+  private final String middleName;
+  private final String lastName;
+  private final String nickname;
+  private final String title;
+  private final String email;
+  private final String notes;
+  private final String month;
+  private final String company;
+  private final String address;
+  private final String birthDay;
+  private final String birthMonth;
+  private final String birthYear;
+  private final String day;
+  private final String homepage;
+  private final String fax;
+  private final String work;
+  private final String mobile;
+  private final String home;
+
+  public UserData(String firstName, String middleName, String lastName, String nickname, String title, String email, String notes, String month, String company, String address, String birthDay, String birthMonth, String birthYear, String day, String homepage, String fax, String work, String mobile, String home) {
+    this.firstName = firstName;
+    this.middleName = middleName;
+    this.lastName = lastName;
+    this.nickname = nickname;
+    this.title = title;
+    this.email = email;
+    this.notes = notes;
+    this.month = month;
+    this.company = company;
+    this.address = address;
+    this.birthDay = birthDay;
+    this.birthMonth = birthMonth;
+    this.birthYear = birthYear;
+    this.day = day;
+    this.homepage = homepage;
+    this.fax = fax;
+    this.work = work;
+    this.mobile = mobile;
+    this.home = home;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getNotes() {
+    return notes;
+  }
+
+  public String getMonth() {
+    return month;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getBirthDay() {
+    return birthDay;
+  }
+
+  public String getBirthMonth() {
+    return birthMonth;
+  }
+
+  public String getBirthYear() {
+    return birthYear;
+  }
+
+  public String getDay() {
+    return day;
+  }
+
+  public String getHomepage() {
+    return homepage;
+  }
+
+  public String getFax() {
+    return fax;
+  }
+
+  public String getWork() {
+    return work;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getHome() {
+    return home;
+  }
+}

db3a66195f3cddf9b71537e5664ffd46dd71d5a5
raik.tatyana <raik.tatyana@gmail.com>
2021-04-27 23:12:58 +0300

    check if a group is created

--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeleteTests.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeleteTests.java
@@ -2,6 +2,7 @@ package ru.stqa.pft.addressbook.tests;
 
 
 import org.testng.annotations.*;
+import ru.stqa.pft.addressbook.model.GroupData;
 
 public class GroupDeleteTests extends TestBase {
 
@@ -9,6 +10,9 @@ public class GroupDeleteTests extends TestBase {
   @Test
   public void testDeleteGroup() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("q", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returnToGroupPage();
--- address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ address-book-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -10,6 +10,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification() throws Exception {
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()) {
+      app.getGroupHelper().createGroup(new GroupData("q", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("q", "test12", "test123"));
